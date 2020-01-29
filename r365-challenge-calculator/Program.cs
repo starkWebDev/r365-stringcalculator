a3a1264999ac1bd02581bae0afffaa0ead82b7a4
Jared Stark <jaredstark@Jareds-MBP-2.app.byu.edu>
2020-01-29 09:05:37 -0700

    Problem 2. Also refactored to much more readable code.

--- r365-challenge-calculator/Program.cs
+++ r365-challenge-calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace r365challengecalculator
 {
@@ -8,47 +9,36 @@ namespace r365challengecalculator
     {
         public static void Main(string[] args)
         {
+            int CheckForValid(string input)
+            {
+                if (Int32.TryParse(input, out _))
+                {
+                    return Int32.Parse(input);
+                }
+                return 0;
+            }
+
             int Calculate(string nums){
                 List<string> inputNums = new List<string>(nums.Split(','));
                 int total;
-                int num1;
-                int num2;
-
-                int CheckForValid(string input)
-                {
-                    if (Int32.TryParse(input, out _))
-                    {
-                        return Int32.Parse(input);
-                    }
-                    return 0;
-                }
 
                 if (inputNums.Count == 0)
                 {
-                    num1 = 0;
-                    num2 = 0;
-                }
-                else if (inputNums.Count == 1)
-                {
-                    num1 = CheckForValid(inputNums[0]);
-                    num2 = 0;
-                }
-                else
-                {
-                    num1 = CheckForValid(inputNums[0]);
-                    num2 = CheckForValid(inputNums[1]);
+                    total = 0;
+                    return (total);
                 }
 
-                total = num1 + num2;
+                IEnumerable<int> convertedNums =  inputNums.Select(input => CheckForValid(input));
+                total = convertedNums.Sum();
 
                 return(total);
             }
 
             void Testing(string input, int expected)
             {
-                Console.WriteLine($"Input: {input}. Expected output: {expected}");
+                Console.WriteLine($"Input: {input}\nExpected output: {expected}");
                 int answer = Calculate(input);
-                Console.WriteLine(answer == expected ? "Correct" : $"Incorrect. Got {answer}");
+                Console.WriteLine(answer == expected ? "Correct\n" : $"Incorrect. Got {answer}\n");
             }
 
             Testing("1,1", 2);
@@ -60,6 +50,13 @@ namespace r365challengecalculator
             Testing("2,2", 4);
             Testing(",", 0);
             Testing("-1,5", 4);
+            Testing("1,2,3", 6);
+            Testing("1,2,3,4", 10);
+            Testing("hi,hi,hi", 0);
+            Testing("hi,1,hi", 1);
+            Testing("1,1,hi", 2);
+            Testing("hi,1,1", 2);
+            Testing(",1,1", 2);
 
         }
     }
